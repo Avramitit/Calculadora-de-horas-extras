0c794ce5ea77dd772ffab8927023b895d9ee95b4
BizarrMan-jpg <39106821+Avramitit@users.noreply.github.com>
2021-03-20 10:49:32 -0300

    Add files via upload

---
+++ Calculadora de horas extras.py
@@ -0,0 +1,35 @@
+def dinero(tarifa, hora):
+	
+	ganancia_total  = (tarifa * hora)
+	
+	return (ganancia_total)
+
+try:
+	
+	ganancia = int(input("¿cuanto cobras por hora? \n"))
+	
+	tiempo = int(input("¿cuanto tiempo trabajas? \n"))
+	
+	
+	if (tiempo) <= (40):
+		
+		resultado = dinero(ganancia, tiempo)
+		
+		print (resultado)
+		
+		
+	elif (tiempo) >= (41):
+		
+		horas_extras = (tiempo - 40)
+		
+		aumento = (ganancia * 1.5)
+		
+		aumento_total = (horas_extras * aumento)
+		
+		resultado = dinero (ganancia, tiempo)
+		
+		print (resultado + aumento_total)
+		
+		
+except:
+	print("solo numeros")
